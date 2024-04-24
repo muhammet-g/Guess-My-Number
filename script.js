bda929e89a257373d252a65cb720a1e656bb958b
muhammet-g <muhammetga642@gmail.com>
2024-04-24 12:27:45 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
+let score = 20;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('⛔ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 correct Number!');
+    document.querySelector('body').style.backgroundColor = '#0bb60b';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😓 You lose a game');
+      score = 0;
+      document.querySelector('body').style.backgroundColor = '#Ff0000';
+    }
+    document.querySelector('.score').textContent = score;
+  }
+  document.querySelector('.guess').value = '';
+  document.querySelector('.guess').focus();
+});
+
+// again button
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
